a4dff12302168b6843c601bb2ce363fd8f9034f6
duct-and-rice <duct@duct>
2017-09-14 12:12:52 +0900

    refactoring

--- _plugins/nch.rb
+++ _plugins/nch.rb
@@ -1,14 +1,20 @@
-require 'cgi'
 require 'open-uri'
 require 'yaml'
 require 'faraday'
 require 'digest/sha2'
+require 'csv'
 module Jekyll
     class Post
-        attr_accessor :header, :body
+        attr_accessor :header, :index, :name, :mail, :metadata, :body
         def initialize(index, name, mail, metadata, body)
-            @header="#{index.to_s} 名前：#{name}[#{mail}] 投稿日：#{metadata}"
+            @index=index
+            @name=name
+            @mail=mail
+            @metadata=metadata
             @body=body
+        end
+        def header
+            "#{index.to_s} 名前：#{name}[#{mail}] 投稿日：#{metadata}"
         end
         def to_liquid
             return { 'header'=>@header,'body'=>@body }
@@ -18,29 +24,21 @@ module Jekyll
     class Thr
         def initialize(url)
             @url=url
+            @digest=Digest::MD5.hexdigest(@url)
+            @path="cache/#{@digest}.csv"
         end
-        def download
-            url=@url
-            url=url.strip.gsub('/$','')
-            digest=Digest::MD5.hexdigest(url)
-            path="dats/#{digest}.dat"
+        def download_dat
+            url=@url.strip.gsub('/$','')
+            part = url.match(/(https?:\/\/.+)\/test\/read\.cgi\/(.+)\/(\d+)\/?/).to_a
+            dat_url = "#{part[1]}/#{part[2]}/dat/#{part[3]}.dat"
             header = {"User-Agent" => "Monazilla/1.00"}
             begin
-                if File.exist?(path) then
-                    body = File.open(path, mode = "r"){|f|
-                        body=f.read 
-                        body
-                    }
-                else
-                    body = open(url, 'r:cp932', header){|w|
-                        body=w.read
-                        body=body.encode(Encoding::UTF_8)
-                        File.open(path, mode = "w"){|f|
-                            f.puts body
-                        }
-                        body
-                    }
-                end
+                puts "  DAT Downloading"
+                body = open(dat_url, 'r:cp932', header,) {|w|
+                               body=w.read
+                               body=body.encode(Encoding::UTF_8)
+                               body
+                }
             rescue OpenURI::HTTPError => e
                 raise DownloadError.new(e.message)
             rescue SystemCallError => e
@@ -48,7 +46,43 @@ module Jekyll
             rescue IOError => e
                 raise IOError.new(e.message)
             end
-            body
+            i=0
+            posts=body.each_line.map { |line|
+                line.chomp!
+                name, mail, metadata, body = line.split('<>')
+                i+=1
+                Post.new(i, name,mail,metadata,body)
+            }
+            posts
+        end
+
+        def load_from_cache
+            begin
+                if File.exist?(@path) then
+                    data = CSV.read(@path, col_sep:"\t", headers:true)
+                    i=0
+                    data=data.map {|res|
+                        Post.new(res[0].to_i, res[1], res[2], res[3], res[4])
+                    }
+                    data
+                else
+                    nil
+                end
+            rescue IOError => e
+                raise IOError.new(e.message)
+            end
+        end
+
+        def save_cache(posts)
+            CSV.open(@path,'wb', col_sep:"\t") {|csv|
+                csv << ['id','name','mail','metadata','body']
+                i=0
+                posts.each {|res|
+                    i+=1
+                    csv << [res.index.to_s, res.name, res.mail, res.metadata, res.body]
+                }
+            }
+            posts
         end
 
         def posts
@@ -56,17 +90,9 @@ module Jekyll
         end
 
         def load_posts
-            body=download
-
-            i=0
-
-            body.each_line.map { |line|
-                line.chomp!
-
-                name, mail, metadata, body = line.split('<>')
-                i+=1
-                Post.new(i, name,mail,metadata,body)
-            }
+            load_from_cache || 
+                save_cache(download_dat)
+            
         end
 
         def to_liquid
@@ -83,9 +109,7 @@ module Jekyll
             @title = title
             @tags = ['yaruo-thread', id].join(' ')
             @name = 'index.html'
-            part = t_url.match(/(https?:\/\/.+)\/test\/read\.cgi\/(.+)\/(\d+)\/?/).to_a
-            dat_url = "#{part[1]}/#{part[2]}/dat/#{part[3]}.dat"
-            @@thread_memo[dat_url] ||= Thr.new(dat_url)
+            @@thread_memo[@url] ||= Thr.new(@url)
 
             self.process(@name)
             self.read_yaml(File.join(base, '_layouts'), 'nch.html')
@@ -110,13 +134,11 @@ module Jekyll
             yml=YAML.load(content)
             url = yml['url']
             range = yml['range']
-            part = url.match(/(https?:\/\/.+)\/test\/read\.cgi\/(.+)\/(\d+)\/?/).to_a
-            dat_url = "#{part[1]}/#{part[2]}/dat/#{part[3]}.dat"
-            reses = Thr.new(dat_url)
-            reses = reses.posts[range[0]-1..range[1]-1]
+            posts = Thr.new(url)
+            posts = posts.posts[range[0]-1..range[1]-1]
 
             tmp=''
-            reses.each_with_index do |res,i|
+            posts.each_with_index do |res,i|
                 tmp << %Q{<dl class="res">\n}
                 tmp << %Q{<dt class="res-header">\n}
                 tmp << res.header
