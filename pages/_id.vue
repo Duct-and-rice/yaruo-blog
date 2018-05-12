3e12aa42d9dc4212a2b0a2e2888d8f1333b86962
Duct-and-rice <nikuinemitsubishi@gmail.com>
2018-05-12 22:15:26 +0900

    fix

---
+++ pages/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{id}}</div>
+</template>
+<script>
+export default {
+  asyncData ({ params }) {
+    return { id: params.id }
+  }
+}
+</script>
---
+++ pages/_year/_month/_day/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{params.id}}</div>
+</template>
+<script>
+export default {
+  validate ({ params }) {
+    return [params.year, params.month, params.day].every(
+      id => /^\d+$/.test(id)
+    )
+  },
+  fetch ({ redirect, params }) {
+    return redirect('/' + params.id)
+  }
+}
+</script>
